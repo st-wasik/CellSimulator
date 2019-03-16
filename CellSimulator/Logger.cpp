c300a2b86083e0ae9d19f27b6ba157387d0e6f3a
Stanisław Wasik <st.wasik@gmail.com>
2019-03-16 20:25:34 +0100

    Changed logger to accept other types, not only std::string

--- CellSimulator/Logger.cpp
+++ CellSimulator/Logger.cpp
@@ -3,11 +3,3 @@
 #include <string>
 
 std::mutex Logger::logMutex;
-
-void Logger::log(const std::string& s, bool endl)
-{
-	std::lock_guard<std::mutex> lock(logMutex);
-	std::clog << s;
-	if (endl) std::clog << std::endl;
-	else std::clog << std::flush;
-}
--- CellSimulator/Logger.h
+++ CellSimulator/Logger.h
@@ -5,7 +5,17 @@ class Logger
 public:
 	Logger() = delete;
 
-	static void log(const std::string&, bool = true);
+	template <typename T>
+	static void log(const T& t, bool endl = true)
+	{
+		std::lock_guard<std::mutex> lock(logMutex);
+		std::clog << t;
+		if (endl)
+			std::clog << std::endl;
+		else
+			std::clog << std::flush;
+	}
+
 private:
 	static std::mutex logMutex;
 };
